764e752ae14653d9934f0e57c36391a25f85faf1
silabs-TungD <tudo@silabs.com>
2025-09-22 11:16:51 +0700

    modify mk file

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 1eca121a37df29064b01eef4f502eb6e22f994f8
+AESD_ASSIGNMENTS_VERSION = f929322628627235e039df37d0168fed04213d70
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
